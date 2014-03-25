2603abecc0aadcc33d4b02e12e6848aba943c3a4
Oleg Vyshnivetsky <olegvish81@gmail.com>
2014-03-25 17:39:15 +0200

    minor polishing

--- WrapLive/Controllers/WLSignUpViewController/WLSignUpViewController.m
+++ WrapLive/Controllers/WLSignUpViewController/WLSignUpViewController.m
@@ -21,25 +21,6 @@
 @property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *stepDoneViews;
 @property (strong, nonatomic) IBOutlet UIView *signUpStepsView;
 
-@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *stepViews;
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (strong, nonatomic) UIDatePicker * birthdatePicker;
-@property (strong, nonatomic) IBOutlet UITextField *phoneNumberTextField;
-@property (strong, nonatomic) IBOutlet UITextField *birthdateTextField;
-@property (strong, nonatomic) WLUser * user;
-@property (strong, nonatomic) IBOutlet UIButton *selectCountryButton;
-@property (strong, nonatomic) IBOutlet UILabel *countryCodeLabel;
-
-@property (strong, nonatomic) IBOutlet UILabel *phoneNumberLabel;
-@property (strong, nonatomic) IBOutlet UITextField *activationTextField;
-
-@property (strong, nonatomic) IBOutlet UIView *inProgressView;
-@property (strong, nonatomic) IBOutlet UILabel *inProgressPhoneLabel;
-@property (strong, nonatomic) IBOutlet UIView *successfulView;
-@property (strong, nonatomic) IBOutlet UILabel *successfulPhoneLabel;
-@property (strong, nonatomic) IBOutlet UIView *failedView;
-@property (strong, nonatomic) IBOutlet UILabel *failedPhoneLabel;
-
 @end
 
 @implementation WLSignUpViewController
@@ -55,7 +36,6 @@
 	[navController willMoveToParentViewController:self];
 	[self addChildViewController:navController];
 	[self.signUpStepsView addSubview:navController.view];
-	
 }
 
 - (void)updateStepLabelsWithIndex:(int)index {
@@ -65,26 +45,12 @@
 		NSUInteger idx = [self.stepLabels indexOfObject:label];
 		label.hidden = idx >= currentStep;
 	}
-	
-	for (UIView* view in self.stepViews) {
-		NSUInteger idx = [self.stepViews indexOfObject:view];
-		view.hidden = idx < currentStep;
-	}
-}
-
-- (void)enlargeScrollOffset {
-	[self.scrollView setContentOffset:CGPointMake(self.scrollView.contentOffset.x + self.scrollView.frame.size.width, self.scrollView.contentOffset.y) animated:YES];
-}
-
-- (void)reduceScrollOffset {
-	[self.scrollView setContentOffset:CGPointMake(self.scrollView.contentOffset.x - self.scrollView.frame.size.width, self.scrollView.contentOffset.y) animated:YES];
 }
 
 #pragma mark - UINavigationControllerDelegate
 
 - (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
 	int index = [navigationController.viewControllers indexOfObject:navigationController.topViewController];
-	NSLog(@"index - %d", index);
 	[self updateStepLabelsWithIndex:index];
 }
 

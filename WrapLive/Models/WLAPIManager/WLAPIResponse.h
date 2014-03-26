fefb680a1b7f7e8c457b6606c34cf82f0036713d
Sergey Maximenko <s.maximenko@mobidev.biz>
2014-03-26 13:26:04 +0200

    splash added

--- WrapLive/Models/WLAPIManager/WLAPIResponse.h
+++ WrapLive/Models/WLAPIManager/WLAPIResponse.h
@@ -13,22 +13,22 @@ typedef NS_ENUM(NSInteger, WLAPIResponseCode) {
 	WLAPIResponseCodeFailure
 };
 
-/**
+/*!
  *  Class, that contains response from the WrapLive API including data, code and message
  */
 @interface WLAPIResponse : JSONModel
 
-/**
+/*!
  *  Dictionary representing the data from the returned response
  */
 @property (strong, nonatomic) NSDictionary* data;
 
-/**
+/*!
  *  Defines status of the response
  */
 @property (nonatomic) NSInteger code;
 
-/**
+/*!
  *  Contains description of the response
  */
 @property (strong, nonatomic) NSString* message;

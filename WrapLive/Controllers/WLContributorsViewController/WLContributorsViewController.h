1f2a543ce9202266c4ad7950465366198a86e3aa
Oleg Vyshnivetsky <olegvish81@gmail.com>
2014-04-07 13:54:13 +0300

    inheritance from shake VC

--- WrapLive/Controllers/WLContributorsViewController/WLContributorsViewController.h
+++ WrapLive/Controllers/WLContributorsViewController/WLContributorsViewController.h
@@ -6,11 +6,11 @@
 //  Copyright (c) 2014 Mobidev. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "WLShakeViewController.h"
 
 @class WLWrap;
 
-@interface WLContributorsViewController : UIViewController
+@interface WLContributorsViewController : WLShakeViewController
 
 @property (nonatomic, strong) WLWrap* wrap;
 
--- WrapLive/Controllers/WLCreateWrapViewController/WLCreateWrapViewController.h
+++ WrapLive/Controllers/WLCreateWrapViewController/WLCreateWrapViewController.h
@@ -6,11 +6,11 @@
 //  Copyright (c) 2014 Mobidev. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "WLShakeViewController.h"
 
 @class WLWrap;
 
-@interface WLCreateWrapViewController : UIViewController
+@interface WLCreateWrapViewController : WLShakeViewController
 
 @property (strong, nonatomic) WLWrap* wrap;
 

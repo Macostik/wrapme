834265f565494e8b8b67af7ece0927567907be26
Sergey Maximenko <s.maximenko@mobidev.biz>
2014-04-25 12:31:29 +0300

    autorotation on full screen image

--- WrapLive/Controllers/WLImageViewController/WLImageViewController.m
+++ WrapLive/Controllers/WLImageViewController/WLImageViewController.m
@@ -11,8 +11,9 @@
 #import "WLCandy.h"
 #import "UIView+Shorthand.h"
 #import "WLSupportFunctions.h"
+#import "WLDeviceOrientationBroadcaster.h"
 
-@interface WLImageViewController () <UIScrollViewDelegate>
+@interface WLImageViewController () <UIScrollViewDelegate, WLDeviceOrientationBroadcastReceiver>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
@@ -34,11 +35,46 @@
 		[weakSelf.spinner removeFromSuperview];
 		weakSelf.scrollView.userInteractionEnabled = YES;
 	}];
+	
+	[self applyDeviceOrientation:[UIDevice currentDevice].orientation animated:NO];
+	
+	[[WLDeviceOrientationBroadcaster broadcaster] addReceiver:self];
 }
 
 - (void)configureScrollViewWithImage:(UIImage*)image {
 	CGSize imageSize = CGSizeMake(image.size.width*image.scale, image.size.height*image.scale);
 	self.scrollView.maximumZoomScale = imageSize.width / CGSizeThatFitsSize(self.view.size, imageSize).width;
+}
+
+- (void)applyDeviceOrientation:(UIDeviceOrientation)orientation animated:(BOOL)animated {
+	CGAffineTransform transform = self.scrollView.transform;
+	if (orientation == UIDeviceOrientationLandscapeLeft) {
+		transform = CGAffineTransformMakeRotation(M_PI_2);
+	} else if (orientation == UIDeviceOrientationLandscapeRight) {
+		transform = CGAffineTransformMakeRotation(3*M_PI_2);
+	} else if (orientation == UIDeviceOrientationPortrait) {
+		transform = CGAffineTransformIdentity;
+	} else if (orientation == UIDeviceOrientationPortraitUpsideDown) {
+		transform = CGAffineTransformMakeRotation(M_PI);
+	}
+	if (!CGAffineTransformEqualToTransform(self.scrollView.transform, transform)) {
+		if (animated) {
+			[UIView beginAnimations:nil context:nil];
+		}
+		self.scrollView.transform = transform;
+		self.scrollView.frame = self.view.bounds;
+		if (animated) {
+			[UIView commitAnimations];
+		}
+	}
+}
+
+#pragma mark - WLDeviceOrientationBroadcastReceiver
+
+- (void)broadcaster:(WLDeviceOrientationBroadcaster *)broadcaster didChangeOrientation:(UIDeviceOrientation)orientation {
+	self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
+	[self applyDeviceOrientation:orientation animated:YES];
+	self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
 }
 
 #pragma mark - Actions

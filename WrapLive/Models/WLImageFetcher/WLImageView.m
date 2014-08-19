6f27e094263fe11eb53783b0a4eac28591c8f0aa
Sergey Maximenko <s.maximenko@mobidev.biz>
2014-08-19 15:52:32 +0300

    [#77192250] perform failure block if image URL is empty

--- WrapLive/Models/WLImageFetcher/WLImageView.m
+++ WrapLive/Models/WLImageFetcher/WLImageView.m
@@ -9,6 +9,7 @@
 #import "WLImageView.h"
 #import "WLImageFetcher.h"
 #import "NSString+Additions.h"
+#import "NSError+WLAPIManager.h"
 
 @interface WLImageView () <WLImageFetching>
 
@@ -28,7 +29,13 @@
 }
 
 - (void)setUrl:(NSString *)url completion:(WLImageFetcherBlock)completion {
+    if (!url.nonempty) {
+        if (completion) completion(nil, NO, [NSError errorWithDescription:@"Image URL is not valid."]);
+        return;
+    }
+    
     _url = url;
+    
 	self.image = nil;
     if (self.contentMode != self.defaultContentMode) {
         self.contentMode = self.defaultContentMode;

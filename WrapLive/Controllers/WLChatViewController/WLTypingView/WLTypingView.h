09898086d53af12881960d918f28488ff686c8a9
Yura Granchenko <yura-granchenko@yandex.ru>
2014-10-15 15:55:29 +0300

    + WLTypingView

---
+++ WrapLive/Controllers/WLChatViewController/WLTypingView/WLTypingView.h
@@ -0,0 +1,13 @@
+//
+//  WLTypingView.h
+//  WrapLive
+//
+//  Created by Yura Granchenko on 10/15/14.
+//  Copyright (c) 2014 Ravenpod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLTypingView : UIView
+
+@end

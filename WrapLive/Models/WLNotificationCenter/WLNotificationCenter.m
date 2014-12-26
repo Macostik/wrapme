39484a78c62568288f4a93da371e164ed74d2765
Calvin So <calvin@outbid.com>
2014-12-26 12:48:59 -0800

    [#85152256] As an engineer, I'd like to ensure the app is using the correct PubNub keys in production / non-production

--- WrapLive/Models/WLNotificationCenter/WLNotificationCenter.m
+++ WrapLive/Models/WLNotificationCenter/WLNotificationCenter.m
@@ -105,14 +105,14 @@ static WLDataBlock deviceTokenCompletion = nil;
     
     if ([WLAPIManager instance].environment.isProduction) {
         origin = @"pubsub.pubnub.com";
-        publishKey = @"pub-c-16ba2a90-9331-4472-b00a-83f01ff32089";
-        subscribeKey = @"sub-c-bc5bfa70-d166-11e3-8d06-02ee2ddab7fe";
-        secretKey = @"sec-c-MzYyMTY1YzMtYTZkOC00NzU3LTkxMWUtMzgwYjdkNWNkMmFl";
-    } else {
-        origin = @"pubsub.pubnub.com";
         publishKey = @"pub-c-87bbbc30-fc43-4f6b-b1f4-cedd5f30d5e8";
         subscribeKey = @"sub-c-6562fe64-4270-11e4-aed8-02ee2ddab7fe";
         secretKey = @"sec-c-NGE5NWU0NDAtZWMxYS00ZjQzLWJmMWMtZDU5MTE3NWE0YzE0";
+    } else {
+        origin = @"pubsub.pubnub.com";
+        publishKey = @"pub-c-16ba2a90-9331-4472-b00a-83f01ff32089";
+        subscribeKey = @"sub-c-bc5bfa70-d166-11e3-8d06-02ee2ddab7fe";
+        secretKey = @"sec-c-MzYyMTY1YzMtYTZkOC00NzU3LTkxMWUtMzgwYjdkNWNkMmFl";
     }
     
 	return [PNConfiguration configurationForOrigin:origin publishKey:publishKey subscribeKey:subscribeKey secretKey:secretKey];

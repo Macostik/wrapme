f92c4395ca207edb7dbccd4b5d1c28683bae807e
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-01-09 11:07:34 +0200

    [#85753700] fixing of new photos notification frequency

--- WrapLive/WLAppDelegate.m
+++ WrapLive/WLAppDelegate.m
@@ -131,7 +131,7 @@
         return;
     }
     [[ALAssetsLibrary library] hasChanges:^(BOOL hasChanges) {
-        if (YES) {
+        if (hasChanges) {
             UILocalNotification *photoNotification = [[UILocalNotification alloc] init];
             photoNotification.alertBody = @"Got new photos? Upload them to your wraps now!";
             photoNotification.fireDate = [[NSDate date] dateByAddingTimeInterval:3];

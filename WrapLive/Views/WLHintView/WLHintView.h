28488e81ee3a6816190996d100c972030ac58699
Yura Granchenko <yura-granchenko@yandex.ru>
2015-03-02 14:32:24 +0200

    [#88860980] refine the suggestion for wrap picker

--- WrapLive/Views/WLHintView/WLHintView.h
+++ WrapLive/Views/WLHintView/WLHintView.h
@@ -5,10 +5,13 @@
 //  Created by Sergey Maximenko on 1/19/15.
 //  Copyright (c) 2015 Ravenpod. All rights reserved.
 //
+#import "WLButton.h"
 
 typedef void (^WLHintViewDrawing)(CGContextRef ctx, CGRect rect);
 
 @interface WLHintView : UIView
+
+@property (weak, nonatomic) IBOutlet WLButton *gotItButton;
 
 @property (strong, nonatomic) IBInspectable UIColor* startColor;
 

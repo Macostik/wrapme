cd9ae890c87007bc705e0edadb130423ebf8f156
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-04-06 18:28:55 +0300

    wrapLiveKit framework made as optional

---
+++ WrapLiveKit/WrapLiveKit.m
@@ -0,0 +1,13 @@
+//
+//  WrapLiveKit.m
+//  WrapLiveKit
+//
+//  Created by Sergey Maximenko on 4/6/15.
+//  Copyright (c) 2015 Ravenpod. All rights reserved.
+//
+
+#import "WrapLiveKit.h"
+
+@implementation WrapLiveKit
+
+@end

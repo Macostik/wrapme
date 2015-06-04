77301c7e7e86831338c7ec148c09aa950ca6e3c5
Yura Granchenko <yura-granchenko@yandex.ru>
2015-06-04 16:37:06 +0300

    [#96174424 #96174054] change placeholder of input box on chat screen and text of placeholder on wrap sreen

--- WrapLive/Controllers/WLChatViewController/WLUnreadMessagesView/WLUnreadMessagesView.m
+++ WrapLive/Controllers/WLChatViewController/WLUnreadMessagesView/WLUnreadMessagesView.m
@@ -17,7 +17,15 @@
 @implementation WLUnreadMessagesView
 
 - (void)setNumberOfUnreadMessages:(NSUInteger)numberOfUnreadMessages {
-    self.textLabel.text = [NSString stringWithFormat:WLLS(@"unread_message"), (unsigned long)numberOfUnreadMessages, numberOfUnreadMessages > 1 ? @"S" : @""];
+    NSString *text = nil;
+    if (numberOfUnreadMessages == 1) {
+        text = [NSString stringWithFormat:WLLS(@"unread_message"), (unsigned long)numberOfUnreadMessages, WLLS(@"oe_Ending"), WLLS(@"ue_Ending")];
+    } else if (numberOfUnreadMessages < 5) {
+        text = [NSString stringWithFormat:WLLS(@"unread_message"), (unsigned long)numberOfUnreadMessages, WLLS(@"sORix_Ending"), WLLS(@"iya_Ending")];
+    } else {
+        text = [NSString stringWithFormat:WLLS(@"unread_message"), (unsigned long)numberOfUnreadMessages, WLLS(@"sORix_Ending"), WLLS(@"ii_Ending")];
+    }
+    self.textLabel.text = text;
 }
 
 @end

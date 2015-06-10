1b2fcfd9df3cd32a7f2786914d4a4ab3b0268f72
Yura Granchenko <yura-granchenko@yandex.ru>
2015-06-10 17:23:46 +0300

    add WLLightSegmentControll to wrap screen

---
+++ WrapLive/Views/WLLIgthSegmentControl/WLLightSegmentControl.h
@@ -0,0 +1,16 @@
+//
+//  WLLightSegmentControl.h
+//  WrapLive
+//
+//  Created by Yura Granchenko on 10/06/15.
+//  Copyright (c) 2015 Ravenpod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLLightSegmentControl : UIButton
+
+@property (strong, nonatomic) IBInspectable UIColor *activeColor;
+@property (strong, nonatomic) IBInspectable UIColor *inActiveColor;
+
+@end

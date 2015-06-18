56064728e4201c42731d3c7fce75ab6f10c7e27d
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-06-18 12:01:12 +0300

    Revert "enabled indexing of identifiers in Core Data"
    This reverts commit a7a18e58ea00aa77cb1898d6d3ef132b299fa472.

--- WrapLiveKit/Models/WLEntryManager/WLEntryManager.m
+++ WrapLiveKit/Models/WLEntryManager/WLEntryManager.m
@@ -161,19 +161,18 @@
 }
 
 - (WLEntry*)entryOfClass:(Class)entryClass identifier:(NSString*)identifier uploadIdentifier:(NSString*)uploadIdentifier {
-    WLEntry *entry = nil;
     if (!uploadIdentifier.nonempty) {
-        entry = [self entryOfClass:entryClass identifier:identifier];
-    } else {
-        entry = [self cachedEntry:identifier];
-        if (!entry && identifier.nonempty) {
-            NSFetchRequest* request = [self fetchRequestForClass:entryClass];
-            request.predicate = [NSPredicate predicateWithFormat:@"identifier == %@ OR uploadIdentifier == %@", identifier, uploadIdentifier];
-            entry = [[request execute] lastObject];
-            if (!entry) {
-                entry = [[entryClass alloc] initWithEntity:request.entity insertIntoManagedObjectContext:self.context];
-                entry.identifier = identifier;
-            }
+        return [self entryOfClass:entryClass identifier:identifier];
+    }
+    WLEntry* entry = [self cachedEntry:identifier];
+    if (!entry) {
+        if (!identifier.nonempty) return nil;
+        NSFetchRequest* request = [self fetchRequestForClass:entryClass];
+        request.predicate = [NSPredicate predicateWithFormat:@"identifier == %@ OR uploadIdentifier == %@", identifier, uploadIdentifier];
+        entry = [[request execute] lastObject];
+        if (!entry) {
+            entry = [[entryClass alloc] initWithEntity:request.entity insertIntoManagedObjectContext:self.context];
+            entry.identifier = identifier;
         }
     }
     return entry;

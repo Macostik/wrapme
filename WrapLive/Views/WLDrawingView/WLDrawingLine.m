626f4ced0c58b648326ad27cf70b22b64670a8b1
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-06-25 14:01:00 +0300

    using better drawing interpolation algorithm from github

--- WrapLive/Views/WLDrawingView/WLDrawingLine.m
+++ WrapLive/Views/WLDrawingView/WLDrawingLine.m
@@ -8,6 +8,7 @@
 
 #import "WLDrawingLine.h"
 #import "WLDrawingBrush.h"
+#import "UIBezierPath+Interpolation.h"
 
 @interface WLDrawingLine ()
 
@@ -41,93 +42,6 @@
             }
         }
     }
-}
-
-double double_interpolate(double p[], double time[], double t) {
-    double L01 = p[0] * (time[1] - t) / (time[1] - time[0]) + p[1] * (t - time[0]) / (time[1] - time[0]);
-    double L12 = p[1] * (time[2] - t) / (time[2] - time[1]) + p[2] * (t - time[1]) / (time[2] - time[1]);
-    double L23 = p[2] * (time[3] - t) / (time[3] - time[2]) + p[3] * (t - time[2]) / (time[3] - time[2]);
-    double L012 = L01 * (time[2] - t) / (time[2] - time[0]) + L12 * (t - time[0]) / (time[2] - time[0]);
-    double L123 = L12 * (time[3] - t) / (time[3] - time[1]) + L23 * (t - time[1]) / (time[3] - time[1]);
-    double C12 = L012 * (time[2] - t) / (time[2] - time[1]) + L123 * (t - time[1]) / (time[2] - time[1]);
-    return C12;
-}
-
-NSMutableArray* interpolateStep(NSArray* points, NSInteger index, NSInteger pointsPerSegment) {
-    NSMutableArray* result = [NSMutableArray array];
-    double x[] = {0,0,0,0};
-    double y[] = {0,0,0,0};
-    double time[] = {0,0,0,0};
-    for (int i = 0; i < 4; i++) {
-        CGPoint point = [points[index + i] CGPointValue];
-        x[i] = point.x;
-        y[i] = point.y;
-        time[i] = i;
-    }
-    
-    double tstart = 1;
-    double tend = 2;
-    
-    double total = 0;
-    for (int i = 1; i < 4; i++) {
-        double dx = x[i] - x[i - 1];
-        double dy = y[i] - y[i - 1];
-        double dtotal = pow(dx * dx + dy * dy, .25);
-        total += dtotal;
-        time[i] = total;
-    }
-    tstart = time[1];
-    tend = time[2];
-    
-    NSInteger segments = pointsPerSegment - 1;
-    [result addObject:points[index + 1]];
-    for (NSInteger i = 1; i < segments; i++) {
-        double xi = double_interpolate(x, time, tstart + (i * (tend - tstart)) / segments);
-        double yi = double_interpolate(y, time, tstart + (i * (tend - tstart)) / segments);
-        [result addObject:[NSValue valueWithCGPoint:CGPointMake(xi, yi)]];
-    }
-    [result addObject:points[index + 2]];
-    return result;
-}
-
-- (NSArray*)interpolatePoints:(NSArray*)currentPoints {
-    
-    if (currentPoints.count < 3) {
-        return currentPoints;
-    }
-    
-    NSInteger segments = 4;
-    
-    NSMutableArray* points = [currentPoints mutableCopy];
-    
-    CGPoint point1 = [points[0] CGPointValue];
-    CGPoint point2 = [points[1] CGPointValue];
-    
-    double x1 = 2*point1.x - point2.x;
-    double y1 = 2*point1.y - point2.y;
-    
-    CGPoint start = CGPointMake(x1, y1);
-    
-    NSInteger n = points.count - 1;
-    point1 = [points[n - 1] CGPointValue];
-    point2 = [points[n] CGPointValue];
-    double xn = 2*point2.x - point1.x;
-    double yn = 2*point2.y - point1.y;
-    CGPoint end = CGPointMake(xn, yn);
-    
-    [points insertObject:[NSValue valueWithCGPoint:start] atIndex:0];
-    
-    [points addObject:[NSValue valueWithCGPoint:end]];
-    
-    NSMutableArray* result = [NSMutableArray array];
-    for (NSInteger i = 0; i < [points count] - 3; i++) {
-        NSMutableArray* stepPoints = interpolateStep(points, i, segments);
-        if ([result count] > 0) {
-            [stepPoints removeObjectAtIndex:0];
-        }
-        [result addObjectsFromArray:stepPoints];
-    }
-    return result;
 }
 
 void CGPathApplierFunc (void *info, const CGPathElement *element) {
@@ -170,21 +84,13 @@ void CGPathApplierFunc (void *info, const CGPathElement *element) {
         p = [point CGPointValue];
         return remove ? nil : point;
     }];
-    points = [NSMutableArray arrayWithArray:[self interpolatePoints:points]];
-    [self.path removeAllPoints];
-    if (points.nonempty) {
-        if (points.count == 1) {
-            [self.path moveToPoint:[points[0] CGPointValue]];
-            [self.path addLineToPoint:[points[0] CGPointValue]];
-        } else {
-            for (NSValue *point in points) {
-                if (point == points.firstObject) {
-                    [self.path moveToPoint:[point CGPointValue]];
-                } else {
-                    [self.path addLineToPoint:[point CGPointValue]];
-                }
-            }
-        }
+    
+    if (points.count > 2) {
+        UIBezierPath *path = [UIBezierPath interpolateCGPointsWithHermite:points closed:NO];
+        path.lineCapStyle = self.path.lineCapStyle;
+        path.lineJoinStyle = self.path.lineJoinStyle;
+        path.lineWidth = self.path.lineWidth;
+        self.path = path;
     }
 }
 

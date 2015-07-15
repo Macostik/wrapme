17fc443d5c8631706af8456f75b93f0c641c77f1
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-07-15 14:06:56 +0300

    [#99106640] Fixed: Tilting the phone on camera screen will cause the icon to rotate

--- WrapLive/Models/WLDeviceOrientationBroadcaster/WLDeviceOrientationBroadcaster.m
+++ WrapLive/Models/WLDeviceOrientationBroadcaster/WLDeviceOrientationBroadcaster.m
@@ -50,38 +50,39 @@
 }
 
 - (void)beginUsingAccelerometer {
+    UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
+    if (orientation != UIDeviceOrientationUnknown) {
+        return;
+    }
     self.motionManager = [[CMMotionManager alloc] init];
     self.motionManager.accelerometerUpdateInterval = 0.5;
     __weak typeof(self)weakSelf = self;
     [self.motionManager startAccelerometerUpdatesToQueue:[[NSOperationQueue alloc] init]
                                         withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
                                             CMAcceleration acceleration = accelerometerData.acceleration;
-                                            CGFloat xx = acceleration.x;
-                                            CGFloat yy = -acceleration.y;
-                                            CGFloat zz = acceleration.z;
-                                            CGFloat device_angle = M_PI / 2.0f - atan2(yy, xx);
-                                            if (device_angle > M_PI)
-                                                device_angle -= 2 * M_PI;
-                                            UIDeviceOrientation orientation = UIDeviceOrientationUnknown;
+
                                             UIDeviceOrientation lastOrientation = [weakSelf.orientationFromAccelerometer integerValue];
-                                            if ((zz >= -.60f) && (zz <= .60f)) {
-                                                if ( (device_angle > -M_PI_4) && (device_angle < M_PI_4) )
-                                                    orientation = UIDeviceOrientationPortrait;
-                                                else if ((device_angle < -M_PI_4) && (device_angle > -3 * M_PI_4))
-                                                    orientation = UIDeviceOrientationLandscapeLeft;
-                                                else if ((device_angle > M_PI_4) && (device_angle < 3 * M_PI_4))
-                                                    orientation = UIDeviceOrientationLandscapeRight;
-                                                else
-                                                    orientation = UIDeviceOrientationPortraitUpsideDown;
+                                            
+                                            float xx = -acceleration.x;
+                                            float yy = acceleration.y;
+                                            float angle = atan2(yy, xx);
+                                            UIDeviceOrientation orientation;
+                                            if(angle >= -2.25 && angle <= -0.25) {
+                                                orientation = UIDeviceOrientationPortrait;
+                                            } else if(angle >= -1.75 && angle <= 0.75) {
+                                                orientation = UIDeviceOrientationLandscapeRight;
+                                            } else if(angle >= 0.75 && angle <= 2.25) {
+                                                orientation = UIDeviceOrientationPortraitUpsideDown;
+                                            } else if(angle <= -2.25 || angle >= 2.25) {
+                                                orientation = UIDeviceOrientationLandscapeLeft;
                                             }
                                             
-                                            if (orientation != UIDeviceOrientationUnknown && orientation != [[UIDevice currentDevice] orientation]) {
-                                                if (orientation != lastOrientation) {
-                                                    weakSelf.orientationFromAccelerometer = @(orientation);
-                                                    run_in_main_queue(^{
-                                                        [weakSelf broadcast:@selector(broadcaster:didChangeOrientation:) object:@(orientation)];
-                                                    });
-                                                }
+                                            if(lastOrientation != orientation) {
+                                                lastOrientation = orientation;
+                                                weakSelf.orientationFromAccelerometer = @(orientation);
+                                                run_in_main_queue(^{
+                                                    [weakSelf broadcast:@selector(broadcaster:didChangeOrientation:) object:weakSelf.orientationFromAccelerometer];
+                                                });
                                             }
                                         }];
 }

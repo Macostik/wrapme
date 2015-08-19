febd8ba1d5f5655d79d78910548db8af0502b043
Sergey Maximenko <s.maximenko@mobidev.biz>
2015-08-19 12:39:19 +0300

    [#101569500] Fixed: UI issues on friends tab

--- Moji/Controllers/WLDataSource/WLEntryCell.m
+++ Moji/Controllers/WLDataSource/WLEntryCell.m
@@ -43,4 +43,10 @@
     [self select:self.entry];
 }
 
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.contentView.frame = self.bounds;
+    self.contentView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+}
+
 @end

2f4d41d2334c7039e9610421d9b7f5cc9e619b0b
Yura Granchenko <yura-granchenko@yandex.ru>
2015-12-25 15:03:01 +0200

    make up welcom screen

--- meWrap/Controllers/WLWelcomeViewController/WLWelcomeViewController.m
+++ meWrap/Controllers/WLWelcomeViewController/WLWelcomeViewController.m
@@ -49,28 +49,14 @@ typedef enum : NSUInteger {
     [super viewDidAppear:animated];
     if (!self.animating) {
         self.animating = YES;
-        [self animateBackgroundView];
     }
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(animateBackgroundView) name:UIApplicationDidBecomeActiveNotification object:nil];
-}
-
-- (void)animateBackgroundView {
-    __weak typeof(self)weakSelf = self;
-    NSTimeInterval duration = 30 * (self.backgroundView.height / 1500.0f);
-    [UIView animateWithDuration:duration delay:0.0f options:UIViewAnimationOptionCurveLinear animations:^{
-        weakSelf.backgroundAnimationPrioritizer.defaultState = !weakSelf.backgroundAnimationPrioritizer.defaultState;
-    } completion:^(BOOL finished) {
-        if (weakSelf.isTopViewController && finished) {
-            [weakSelf animateBackgroundView];
-        }
-    }];
 }
 
 - (void)underlineLicenseButton {
 	NSMutableAttributedString *titleString = [[NSMutableAttributedString alloc] initWithString:@"terms_and_conditions".ls];
 	NSDictionary * attributes = @{NSUnderlineStyleAttributeName : [NSNumber numberWithInteger:NSUnderlineStyleSingle],
 								  NSFontAttributeName : [UIFont fontSmall],
-								  NSForegroundColorAttributeName : Color.orange};
+								  NSForegroundColorAttributeName : [UIColor whiteColor]};
 	[titleString addAttributes:attributes range:NSMakeRange(0, [titleString length])];
 	[self.licenseButton setAttributedTitle: titleString forState:UIControlStateNormal];
 }
@@ -96,8 +82,10 @@ typedef enum : NSUInteger {
     __weak typeof(self)weakSelf = self;
     [[Dispatch defaultQueue] fetch:^id _Nullable{
         NSURL *url = [[NSBundle mainBundle] URLForResource:@"Wraplive_TermsAndConditions" withExtension:@"rtf"];
-        return [[NSAttributedString alloc] initWithURL:url options:@{} documentAttributes:nil error:nil];
+        return [[NSMutableAttributedString alloc] initWithURL:url options:@{} documentAttributes:nil error:nil];
     } completion:^(id object) {
+        NSDictionary *attributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+        [object addAttributes:attributes range:NSMakeRange(0, [object length])];
         weakSelf.termsAndConditionsTextView.attributedText = object;
     }];
 }

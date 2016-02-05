7bd0ede4ad00cc96a370d0c316062145341efeed
Sergey Maximenko <s.maximenko@mobidev.biz>
2016-02-05 13:55:49 +0200

    [#113142471] Fixed: Viewer's resolution (and view) becomes weird when broadcaster is using landscape to do broadcasting

--- meWrap/Controllers/LiveViewerViewController.swift
+++ meWrap/Controllers/LiveViewerViewController.swift
@@ -70,6 +70,13 @@ class LiveViewerViewController: LiveViewController {
             }
         }
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        UIView.performWithoutAnimation { [unowned self] () -> Void in
+            self.playerLayer?.frame = self.view.bounds
+        }
+    }
 
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         guard let keyPath = keyPath, let item = playerItem else { return }

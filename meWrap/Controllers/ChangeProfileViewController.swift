49d9b82a889af6b7e0341d3375be70420808a45d
Yura Granchenko <yura-granchenko@yandex.ru>
2016-04-20 16:47:18 +0300

    [#117997557] Fixed: Blank white field is displayed in 'Edit Profile' window if user hasn't chosen a photo yet

--- meWrap/Controllers/ChangeProfileViewController.swift
+++ meWrap/Controllers/ChangeProfileViewController.swift
@@ -238,6 +238,9 @@ final class ChangeProfileViewController: BaseViewController, EditSessionDelegate
         }
         
         imageView.cornerRadius = 78
+        imageView.defaultIconText = "&"
+        imageView.defaultIconColor = Color.grayLighter
+        imageView.defaultBackgroundColor = UIColor.whiteColor()
         headerView.addSubview(imageView)
         imageView.snp_makeConstraints {
             $0.size.equalTo(156)
